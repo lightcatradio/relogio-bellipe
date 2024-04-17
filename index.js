09dec155149cabcb4c871d59933429341ef5acc0
Felipe M. Garcia <felipegarcia62@yahoo.com>
2024-04-17 15:12:36 -0300

    retirado o contador de semanas

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ function calcularDiferencaDeTempo(dataInicial, dataFinal) {
   const segundosPorMinuto = 60;
   const segundosPorHora = segundosPorMinuto * 60;
   const segundosPorDia = segundosPorHora * 24;
-  const segundosPorSemana = segundosPorDia * 7;
   const segundosPorMes = segundosPorDia * 30;
   const segundosPorAno = segundosPorDia * 365;
 
   const anos = Math.floor(segundosTotais / segundosPorAno);
   const meses = Math.floor(segundosTotais / segundosPorMes);
-  const semanas = Math.floor(segundosTotais / segundosPorSemana);
   const dias = Math.floor(segundosTotais / segundosPorDia) % 30;
   const horas = Math.floor((segundosTotais % segundosPorDia) / segundosPorHora);
   const minutos = Math.floor(
@@ -22,7 +20,6 @@ function calcularDiferencaDeTempo(dataInicial, dataFinal) {
   return {
     anos,
     meses,
-    semanas,
     dias,
     horas,
     minutos,
@@ -33,36 +30,32 @@ function calcularDiferencaDeTempo(dataInicial, dataFinal) {
 function formatarTempo(tempo) {
   const doisDigitos = (valor) => String(valor).padStart(2, "0");
   return `${doisDigitos(tempo.anos)}:${doisDigitos(tempo.meses)}:${doisDigitos(
-    tempo.semanas
-  )}:${doisDigitos(tempo.dias)}:${doisDigitos(tempo.horas)}:${doisDigitos(
-    tempo.minutos
-  )}:${doisDigitos(tempo.segundos)}`;
+    tempo.dias
+  )}:${doisDigitos(tempo.horas)}:${doisDigitos(tempo.minutos)}:${doisDigitos(
+    tempo.segundos
+  )}`;
 }
 
 function formatarTempoMobile(tempo) {
   const doisDigitos = (valor) => String(valor).padStart(2, "0");
   const anosEMeses = `${doisDigitos(tempo.anos)}:${doisDigitos(tempo.meses)}`;
-  const semanasEDias = `${doisDigitos(tempo.semanas)}:${doisDigitos(
-    tempo.dias
+  const diasEHoras = `${doisDigitos(tempo.dias)}:${doisDigitos(tempo.horas)}`;
+  const minutosESegundos = `${doisDigitos(tempo.minutos)}:${doisDigitos(
+    tempo.segundos
   )}`;
-  const horasEMinutos = `${doisDigitos(tempo.horas)}:${doisDigitos(
-    tempo.minutos
-  )}`;
-  const segundosESegundos = `${doisDigitos(tempo.segundos)}`;
 
   return {
     anosEMeses,
-    semanasEDias,
-    horasEMinutos,
-    segundosESegundos,
+    diasEHoras,
+    minutosESegundos,
   };
 }
 
 function atualizarTempo() {
   const tempoElemento = document.getElementById("tempo");
   const elementoAnosEMeses = document.getElementById("anos-meses");
-  const elementoSemanasEDias = document.getElementById("semanas-dias");
-  const elementoHorasEMinutos = document.getElementById("horas-minutos");
+  const elementoDiasEHoras = document.getElementById("dias-horas");
+  const elementoMinutosESegundos = document.getElementById("minutos-segundos");
   const elementoSegundos = document.getElementById("segundos");
   const dataInicial = new Date("2024-03-18T00:00:00");
   const dataAtual = new Date();
@@ -71,9 +64,8 @@ function atualizarTempo() {
   const tempoFormatadoMobile = formatarTempoMobile(diferencaDeTempo);
   tempoElemento.textContent = tempoFormatado;
   elementoAnosEMeses.textContent = tempoFormatadoMobile.anosEMeses;
-  elementoSemanasEDias.textContent = tempoFormatadoMobile.semanasEDias;
-  elementoHorasEMinutos.textContent = tempoFormatadoMobile.horasEMinutos;
-  elementoSegundos.textContent = tempoFormatadoMobile.segundosESegundos;
+  elementoDiasEHoras.textContent = tempoFormatadoMobile.diasEHoras;
+  elementoMinutosESegundos.textContent = tempoFormatadoMobile.minutosESegundos;
 }
 
 setInterval(atualizarTempo, 1000);
